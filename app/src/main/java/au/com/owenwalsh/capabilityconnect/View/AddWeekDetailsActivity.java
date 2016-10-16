0e868afbca8fc90c37e55de7a3cc9a0c613990a7
owen walsh <owen walsh>
2016-10-16 20:12:49 +1100

    adding required views uncomplete

---
+++ app/src/main/java/au/com/owenwalsh/capabilityconnect/View/AddWeekDetailsActivity.java
@@ -0,0 +1,13 @@
+package au.com.owenwalsh.capabilityconnect.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddWeekDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_week_details);
+    }
+}
